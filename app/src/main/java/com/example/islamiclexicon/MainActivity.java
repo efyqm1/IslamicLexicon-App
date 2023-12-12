f8c31e04546d6f1359f774178731d19b824d9798
efyqm1 <efyqm1@nottingham.edu.my>
2023-12-12 22:18:41 +0800

    Started implementing SearchBar with com.google.android.material.search.SearchBar

--- app/src/main/java/com/example/islamiclexicon/MainActivity.java
+++ app/src/main/java/com/example/islamiclexicon/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.islamiclexicon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.islamiclexicon.Adapter.SearchAdapter;
+import com.example.islamiclexicon.Database.Database;
+import com.google.android.material.search.SearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    SearchAdapter adapter;
+
+    com.google.android.material.search.SearchBar searchBar;
+    List<String> suggestList = new ArrayList<>();
+
+    Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init View
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_search);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        searchBar  = (com.google.android.material.search.SearchBar) findViewById(R.id.search_bar);
+
+        //Init DB
+        database = new Database(this);
+
+        //Setup Search bar
+        searchBar.setHint("Search");
+        loadSuggestList();
+        searchBar.setOnS
+
+    }
+
+    private void loadSuggestList() {
+        suggestList = database.getArabic_word();
+
     }
 }
