0845cec667ee2482070088395907a1e54cbd70f9
efyqm1 <efyqm1@nottingham.edu.my>
2023-12-13 15:51:41 +0800

    Update IslamicWordTest.java

--- app/src/test/java/com/example/islamiclexicon/IslamicWordTest.java
+++ app/src/test/java/com/example/islamiclexicon/IslamicWordTest.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.*;
 
 import com.example.islamiclexicon.Model.IslamicWord;
 
+/**
+ * JUnit tests for the IslamicWord class.
+ */
 public class IslamicWordTest {
 
+    /**
+     * Test getters and setters of the IslamicWord class.
+     */
     @Test
     public void testGettersAndSetters() {
         IslamicWord word = new IslamicWord();
@@ -23,6 +29,31 @@ public class IslamicWordTest {
         assertEquals("TestArabicRootWord", word.getArabic_root_word());
     }
 
-    // Add more tests based on specific methods or behavior in the IslamicWord class.
-}
+    /**
+     * Test the empty constructor of the IslamicWord class.
+     */
+    @Test
+    public void testEmptyConstructor() {
+        IslamicWord word = new IslamicWord();
+        assertNull(word.getArabic_word());
+        assertNull(word.getTransliteration());
+        assertNull(word.getLiteral_translation());
+        assertNull(word.getTechnical_meaning());
+        assertNull(word.getArabic_root_word());
+    }
 
+    /**
+     * Test the non-null constructor of the IslamicWord class.
+     */
+    @Test
+    public void testNonNullConstructor() {
+        IslamicWord word = new IslamicWord("Word", "Translit", "Literal", "Tech", "Root");
+        assertNotNull(word);
+        assertEquals("Word", word.getArabic_word());
+        assertEquals("Translit", word.getTransliteration());
+        assertEquals("Literal", word.getLiteral_translation());
+        assertEquals("Tech", word.getTechnical_meaning());
+        assertEquals("Root", word.getArabic_root_word());
+    }
+
+}
